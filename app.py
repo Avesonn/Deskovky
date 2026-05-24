79bfdbbde4140aaf6cbf0b8e11bc10490bad035e
Avesonn <Lord.of.xantix@seznam.cz>
2026-05-24 14:54:11 +0200

    Refactor Streamlit app layout and visibility settings
    Updated styling to hide specific Streamlit elements and maintain menu visibility.

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ st.set_page_config = lambda *args, **kwargs: None
 # ==========================================
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
+            /* Skryje jen prvky, které nepatří do menu */
+            #MainMenu {visibility: visible;} 
             footer {visibility: hidden;}
             header {visibility: hidden;}
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            [data-testid="stDecoration"] {visibility: hidden !important;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
